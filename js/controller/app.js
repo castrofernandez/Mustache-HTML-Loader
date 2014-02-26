de223e6ea414d36b0d089a9ff46637e27a84389d
Juan Castro Fernández <castrofernandez@gmail.com>
2014-02-26 20:44:18 +0100

    Partials and internationalization

--- js/controller/app.js
+++ js/controller/app.js
@@ -1,15 +1,55 @@
-var template = document.querySelector('script[type=mustache]');
+(function loadPartials() {
+	var partials = document.querySelectorAll('script[type=mustache-partial]');
+	
+	var partialContent = new Object();
+	
+	var count = partials.length;
+	
+	for (var i = 0; i < partials.length; i++)
+		loadFile(partials[i].src, function(file, request) {
+			count--;
+			
+			var name = file.substring(file.lastIndexOf('/') + 1);
+			name = name.substring(0, name.indexOf('.'));
+			
+			partialContent[name] = request.responseText;
+	
+			if (count <= 0)
+				loadInternationalization(partialContent)
+		});
+})();
 
-var xmlhttp = new XMLHttpRequest();
+function loadInternationalization(partials) {
+	var lang = document.querySelector('script[type=lang]');
+	
+	loadFile(lang.src, function(file, request) {
+		var labels = JSON.parse(request.responseText);
+	
+		loadTemplate(partials, labels);
+	});
+}
 
-xmlhttp.onreadystatechange = function() 
-{
-	if (xmlhttp.readyState == 4) 
-	{ 
-		document.body.innerHTML = Mustache.render(xmlhttp.responseText, data);
+function loadTemplate(partials, labels) {
+	var template = document.querySelector('script[type=mustache]');
+				
+	data.labels = labels;
+				
+	loadFile(template.src, function(file, request) {
+		document.body.innerHTML = Mustache.render(request.responseText, data, partials);
+	});
+}
+
+function loadFile(file, callback) {
+	var request = new XMLHttpRequest();
+	
+	request.onreadystatechange = function() 
+	{
+		if (request.readyState == 4) 
+		{ 
+			callback(file, request);
+		} 
 	} 
-} 
-
-xmlhttp.open("GET", template.src, true); 
-xmlhttp.send();
-
+	
+	request.open("GET", file, true); 
+	request.send();
+}
